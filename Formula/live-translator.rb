1eed304719c8146db0d4365acc47d6906e94f812
mikeasm <mikeasm48@gmail.com>
2026-09-21 08:42:52 +0500

    Обновить до 0.4.7

--- Formula/live-translator.rb
+++ Formula/live-translator.rb
@@ -1,15 +1,15 @@
 class LiveTranslator < Formula
   desc "Live speech translation (Uzbek/Russian/English) via Gemini"
   homepage "https://github.com/mikeasm48/live_translator"
-  url "https://github.com/mikeasm48/live_translator/releases/download/v0.4.6/live-translator-0.4.6.jar",
+  url "https://github.com/mikeasm48/live_translator/releases/download/v0.4.7/live-translator-0.4.7.jar",
       using: :nounzip
-  sha256 "8faca432354f0437329e785b2e96f35c13028e6036bbd5a6ed9adb1cf279ae20"
-  version "0.4.6"
+  sha256 "a371300696fd3572c438417f1b1d5e2564549e773277717e88d4f88f71f641cb"
+  version "0.4.7"
 
   depends_on "openjdk@21"
 
   def install
-    libexec.install "live-translator-0.4.6.jar" => "live-translator.jar"
+    libexec.install "live-translator-0.4.7.jar" => "live-translator.jar"
 
     # Обёртка вызывает Java из зависимости формулы, поэтому системная версия
     # Java ни на что не влияет и ставить её отдельно не нужно.
